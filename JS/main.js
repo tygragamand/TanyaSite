5b1f7a2a6d24eb3deb6b56ad0ba7a51c2b7fc62b
Tatiana <tygragamand@users.noreply.github.com>
2018-10-18 13:51:32 -0400

    test2

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,11 @@
 function test1() {
 	var firstname = prompt ("Hey what's your name?");
 	var mycolor = prompt ("Pick a color, " + firstname);
+
+	if (mycolor == "red"){
+		console.log("ha. this guy likes red");
+	}
+	else if (mycolor == "blue") {
+		console.log("blue, my favourite color");
+	}
 }
